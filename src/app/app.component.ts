e96486422e45d2e558f10fd9e124c56dceb3f582
Karthik Saxena <katik.ks@gmail.com>
2021-01-19 19:56:30 +0530

    FormsCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormControl,FormGroup} from '@angular/forms'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'sample';
+export class AppComponent implements OnInit  {
+  // title = 'Enter Username';
+  
+  // name: string = 'default Name';
+  
+  loginForm: FormGroup;
+
+//Angular Component LifeCycle
+
+emailId: string;
+
+password: string;
+
+  ngOnInit(){
+
+    //Initialization
+
+      this.loginForm = new FormGroup({
+        emailId : new FormControl(),
+        password: new FormControl()
+      });
+  }
+
+  // updateValue() {
+  //   this.name ='Changed Name';
+  // }
+
+  submit(data){
+    console.log(data);
+  }
 }
