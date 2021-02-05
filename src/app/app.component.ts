66d91eb3cea54f009694bdf538d32e70d2bdd7e1
Karthik Saxena <katik.ks@gmail.com>
2021-02-05 19:14:33 +0530

    0402

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl,FormGroup} from '@angular/forms'
-
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit  {
+export class AppComponent implements OnInit {
   // title = 'Enter Username';
-  
+
   // name: string = 'default Name';
-  
+
   loginForm: FormGroup;
+  submitted = false;
 
-//Angular Component LifeCycle
+  //Angular Component LifeCycle
 
-emailId: string;
+  emailId;
 
-password: string;
+  password;
 
-  ngOnInit(){
-
+  ngOnInit() {
     //Initialization
+    //FormBuilder
+    this.loginForm = new FormGroup({
+      emailId: new FormControl('', Validators.required),
+      password: new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(20)]),
+    });
+  }
 
-      this.loginForm = new FormGroup({
-        emailId : new FormControl(),
-        password: new FormControl()
-      });
+  get f() {
+    return this.loginForm.controls;
   }
 
   // updateValue() {
   //   this.name ='Changed Name';
   // }
 
-  submit(data){
+  submit(data) {
+    this.submitted = true;
+
+    if(this.loginForm.invalid){
+      return;
+    }
+
     console.log(data);
   }
 }
